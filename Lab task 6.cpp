3994b82c2496872abe6ab57376b895b5effc0036
easyscenez <147534619+easyscenez@users.noreply.github.com>
2023-11-28 22:56:16 +0500

    Add files via upload

---
+++ Lab task 6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	int n=0, m=1, sum=0;
+//	int rows;
+//	cout<<"Enter the number of rows for which the fibonacci sequence will be generated."<<endl;
+//	cin>>rows;
+//	for (int i=1; i<=rows; i++){
+//		n=0, m=1, sum=0;
+//		for(int j=0;j<i; j++){
+//			cout<<n<<" ";
+//			sum=n+m;
+//			n=m;
+//			m=sum;
+//			
+//		}
+//		cout<<endl;
+//	}
+	// end of lab task 1
+	
+//	int rows, n=1;
+//	cout<<"Enter the number of rows for the floyd triangle to be displayed."<<endl;
+//	cin>>rows;
+//	for(int i=1;i<=rows;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<n<<" ";
+//			n++;
+//		}
+//		cout<<endl;
+//	}
+	// end of lab task 2
+	
+}
