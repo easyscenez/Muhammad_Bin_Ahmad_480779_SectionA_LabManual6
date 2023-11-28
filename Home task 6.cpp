fb9a21588e4aac8998aeef7731786c9bc452a6e8
easyscenez <147534619+easyscenez@users.noreply.github.com>
2023-11-28 22:46:37 +0500

    Add files via upload

---
+++ Home task 6.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//	int sum=0;
+//	bool prime;
+//	for (int i=2;i<=50;i++){
+//		prime=true;
+//		for(int j=2;j<i;j++){
+//			if(i%j==0){
+//				prime=false;
+//				break;
+//				}
+//			}
+//		if(prime==true){
+//		sum+=i;	
+//		}
+//	}
+//		cout<<"The sum of all prime numbers from 1 to 50 is "<<sum;
+	//end of home task 1	
+	
+//	for(int r=1; r<=5; r++){
+//		for(int i=1;i<=r;i++){
+//			cout<<i<<" ";
+//			
+//		}
+//		cout<<endl;
+//	}
+	// end of home task 2
+	
+	
+//	int num=1;
+//	cout<<num;
+//	for(int r=0; r<=6; r+=2){
+//		for(int i=1;i<=r;i++){
+//			cout<<r<<" ";
+//			
+//		}
+//		cout<<endl;
+//	}
+	// end of home task 3
+
+}
